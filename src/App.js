aa3ad435d3953c97a421670f84065519abd60a24
Marsiya <67198163+Mar-Issah@users.noreply.github.com>
2021-10-05 12:11:09 -0400

    built navbar routes and components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
-import { Navbar } from "./components/Navbar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+import Donate from "./components/donate/Donate";
+import Profile from "./components/profile/Profile";
+import Blog from "./components/blog/Blog";
+import AddResource from "./components/add-resource/AddResource";
+import Search from "./components/search/Search";
+import Logout from "./components/logout/Logout";
 
 const App = () => {
 	return (
-		<div>
+		<Router>
 			<Navbar />
-		</div>
+			<br />
+			<Route path="/" exact component={Home} />
+			<Route path="/donate" component={Donate} />
+			<Route path="/profile" component={Profile} />
+			<Route path="/blog" component={Blog} />
+			<Route path="/add-resource" component={AddResource} />
+			<Route path="/search" component={Search} />
+			<Route path="/logout" component={Logout} />
+		</Router>
 	);
 };
 
