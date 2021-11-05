8ed25cc0294e84af88d8270e2456ec575db511e4
Mar-Issah <masy370@gmail.com>
2021-11-04 22:42:36 -0400

    commit untracked file

---
+++ src/Global.Styled.js
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+.dropdown-menu {
+	position: absolute !important;
+	left: 0 !important;
+	width: 100% !important;
+	background-color: #4c4695 !important;
+	border-radius: 0 !important;
+}
+
+.nav-link {
+	color: #ffffff !important;
+	letter-spacing: 34;
+	padding: 0.5rem 0;
+}
+
+.offcanvas-body svg {
+	width: 1.125rem !important;
+	height: 1em;
+	margin-left: 20px;
+}
+
+a[aria-expanded="true"] {
+	background-color: #4c4695;
+	width: 100%;
+	margin-bottom: 190px !important;
+}
+
+svg {
+	stroke: blue;
+	fill: white !important;
+}
+`;
+
+export default GlobalStyles;
