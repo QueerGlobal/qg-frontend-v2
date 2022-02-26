8cb927c9afccc7a5981cc0401b5315cc9acff1fe
Marsha Annon”git config --global user.name Marsha <marshaannon@gmail.com>
2022-02-25 19:48:02 -0500

    Links to resources section now more responsive

--- src/components/homepageLinks/HomepageLinks.css.ts
+++ src/components/homepageLinks/HomepageLinks.css.ts
@@ -2,17 +2,23 @@ import styled from 'styled-components';
 import colors from '../../styles/colors';
 
 export const LinksWrapperProps = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
   background-color: ${colors.WHITE};
   padding: 15px;
   width: 95%;
 `;
 
 export const Header = styled.h1`
+  grid-column: 1 / 3;
   color: ${colors.BLACK};
-  font-size: 24px;
+  font-size: 1.5rem;
+  font-weight: 800;
+  padding-top: 16px;
   text-transform: uppercase;
+`;
+
+export const ResoucesWrapper = styled.div`
+  grid-column: 2 / 3;
+  margin: 0 auto;
 `;
--- src/components/homepageLinks/HomepageLinks.tsx
+++ src/components/homepageLinks/HomepageLinks.tsx
@@ -1,16 +1,22 @@
 import React, { FC } from 'react';
 
 import Button from '../common/button/Button';
-import { LinksWrapperProps, Header } from './HomepageLinks.css';
+import {
+  LinksWrapperProps,
+  Header,
+  ResoucesWrapper,
+} from './HomepageLinks.css';
 
 const HomepageLinks: FC = () => {
   return (
     <LinksWrapperProps>
-    <Header>Find what you need</Header>
-      <Button label='primary' buttonText='resources'/>
-      <Button label='primary' buttonText='businesses' />
-      <Button label='primary' buttonText='events' />
-      <Button label='secondary' buttonText='support us' />
+      <Header>Find what you need</Header>
+      <ResoucesWrapper>
+        <Button label='primary' buttonText='resources' />
+        <Button label='primary' buttonText='businesses' />
+        <Button label='primary' buttonText='events' />
+        <Button label='secondary' buttonText='support us' />
+      </ResoucesWrapper>
     </LinksWrapperProps>
   );
 };
