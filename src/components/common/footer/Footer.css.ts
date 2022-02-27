d3a3eca0af4795fc62e43547c01640f77fc65860
Marsha Annon”git config --global user.name Marsha <marshaannon@gmail.com>
2022-02-27 17:18:58 -0500

    now homepage is mopre responsive

--- src/components/common/footer/Footer.css.ts
+++ src/components/common/footer/Footer.css.ts
@@ -1,74 +1,75 @@
-import styled from "styled-components";
-import { Container, Nav } from "react-bootstrap";
-import colors from "../../../styles/colors";
+import styled from 'styled-components';
+import { Container, Nav } from 'react-bootstrap';
+import colors from '../../../styles/colors';
 
 export const FooterContainer = styled(Container)`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	background-color: ${colors.BLACK};
-	height: 100%;
-	max-width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: ${colors.BLACK};
+  height: 100%;
+  max-width: 100%;
+  align-self: stretch;
 `;
 
 export const FooterBar = styled.div`
-	height: 22px;
-	margin: 0 -20px;
-	background: transparent
-		linear-gradient(244deg, #c69c6d 0%, #754c24 48%, #c69c6d 76%, #754c24 100%)
-		0% 0% no-repeat;
-	opacity: 1;
-	align-self: stretch;
+  height: 22px;
+  margin: 0 -20px;
+  background: transparent
+    linear-gradient(244deg, #c69c6d 0%, #754c24 48%, #c69c6d 76%, #754c24 100%)
+    0% 0% no-repeat;
+  opacity: 1;
+  align-self: stretch;
 `;
 
 export const LogoContainer = styled.div`
-	margin: 40px auto;
+  margin: 40px auto;
 `;
 
 export const KnowUs = styled.p`
-	height: 19px;
-	font-size: 14px;
-	text-transform: uppercase;
-	color: #ffffff;
-	opacity: 1;
-	font-weight: bold;
+  height: 19px;
+  font-size: 14px;
+  text-transform: uppercase;
+  color: #ffffff;
+  opacity: 1;
+  font-weight: bold;
 `;
 
 export const Email = styled.p`
-	height: 19px;
-	text-align: center;
-	color: #ffffff;
-	opacity: 1;
-	letter-spacing: 0.14px;
-	font-weight: bold;
-	margin-top: 30px;
+  height: 19px;
+  text-align: center;
+  color: #ffffff;
+  opacity: 1;
+  letter-spacing: 0.14px;
+  font-weight: bold;
+  margin-top: 30px;
 `;
 
 export const NavLinksContainer = styled.div`
-	margin-top: 20px;
-	margin-bottom: 40px;
+  margin-top: 20px;
+  margin-bottom: 40px;
 `;
 
 export const NavLink = styled(Nav.Link)`
-	color: #ffffff;
-	font-size: 18px;
-	text-align: center;
-	opacity: 1;
+  color: #ffffff;
+  font-size: 18px;
+  text-align: center;
+  opacity: 1;
 
-	&:focus,
-	&:active {
-		color: #ffffff;
-	}
+  &:focus,
+  &:active {
+    color: #ffffff;
+  }
 `;
 export const FooterLinks = styled.p`
-	text-align: center;
-	color: #ffffff;
-	font-size: 18px;
+  text-align: center;
+  color: #ffffff;
+  font-size: 18px;
 `;
 export const RightsReserved = styled.p`
-	font-weight: 100;
-	height: 17px;
-	color: #fff;
-	text-align: center;
+  font-weight: 100;
+  height: 17px;
+  color: #fff;
+  text-align: center;
 `;
--- src/components/homepageCTA2/HomepageCTA2.tsx
+++ src/components/homepageCTA2/HomepageCTA2.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, { FC } from 'react';
 
 import Button from '../common/button/Button';
 import {
@@ -16,38 +16,47 @@ import {
   Span,
   ButtonWrapper,
   Content,
-} from './HomepageCTA2.css'
+} from './HomepageCTA2.css';
+import BeautifulFlower from '../../assets/beautifulFlower.jpg';
 
 const HomepageCTA2: FC = () => {
   return (
     <HomepageCTA2Wrapper>
       <ImageWrapper1>
-        <Image>Image goes here</Image>
+        <Image />
       </ImageWrapper1>
-      <Header>You're face is lovely, <br />
-              can we use it?
+      <Header>
+        You're face is lovely, <br />
+        can we use it?
       </Header>
       <Content>
-        We believe every face, every body, and every differently-abled or colored human is beautiful just as they are, and are worthy of representation as such. <br /><br />
-      
-        For that reason, all the people you see on our site are real Queer Global members. If you'd like to be a face of QG, send us your photo! <br /><br />
-        
+        We believe every face, every body, and every differently-abled or
+        colored human is beautiful just as they are, and are worthy of
+        representation as such. <br />
+        <br />
+        For that reason, all the people you see on our site are real Queer
+        Global members. If you'd like to be a face of QG, send us your photo!{' '}
+        <br />
+        <br />
         We'd be happy to have you!
       </Content>
       <ButtonWrapper>
-        <Button label="primary" buttonText='Send us your photo' />
+        <Button label='primary' buttonText='Send us your photo' />
       </ButtonWrapper>
       <SubHeader>A place for</SubHeader>
-      <SubHeader1><Span>you</Span>,</SubHeader1>
+      <SubHeader1>
+        <Span>you</Span>,
+      </SubHeader1>
       <SubHeader2>you beautiful</SubHeader2>
       <SubHeader3>human.</SubHeader3>
 
       <ImageWrapper2>
+        {/* <BeautifulFlower /> */}
         <Image1>Image goes here</Image1>
         <Image2>Image goes here</Image2>
       </ImageWrapper2>
     </HomepageCTA2Wrapper>
   );
-}
+};
 
 export default HomepageCTA2;
