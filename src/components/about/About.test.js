0a687430e2b5dfa96504fc8d5b372c851549061d
Meke Brown <mekesiabrown@gmail.com>
2022-05-19 19:30:28 -0400

    Reset code

--- src/components/about/About.test.js
+++ src/components/about/About.test.js
@@ -6,19 +6,3 @@ test('renders about text', () => {
   const aboutText = screen.getByText(/ABOUT US/i);
   expect(aboutText).toBeInTheDocument();
 });
-
-test('renders about page copy', () => {
-  render(<About />);
-
-  const andWeBelieveBodyText = screen.getByText(/And we believe that kind of diversity is what makes Queerness beautiful./i);
-  expect(andWeBelieveBodyText).toBeInTheDocument();
-
-  const weAreHereForHeaderText = screen.getByText(/WE ARE HERE FOR/i);
-  expect(weAreHereForHeaderText).toBeInTheDocument();
-  
-  const sendUsBtnText = screen.getByText(/SEND US YOUR PHOTO OR ARTWORK/i);
-  expect(sendUsBtnText).toBeInTheDocument();
-  
-  const addAResourceBtnText = screen.getByText(/ADD A RESOURCE/i);
-  expect(addAResourceBtnText).toBeInTheDocument();
-});
