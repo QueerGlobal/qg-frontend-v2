c5771ee44435df7b0a6d10f1c4a95d680bd7bc04
Meke Brown <mekesiabrown@gmail.com>
2022-05-19 11:28:58 -0400

    Starting proper test coverage

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders support us link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/SUPPORT US/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders home page heading', () => {
+  render(<App />);
+  const homePageText = screen.getByText(/WE'RE QUEER GLOBAL/i);
+  expect(homePageText).toBeInTheDocument();
 });
