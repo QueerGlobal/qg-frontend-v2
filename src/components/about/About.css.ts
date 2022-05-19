cc9e70d63b74dbb725b76c9618bbeba9a4babbe2
Meke Brown <mekesiabrown@gmail.com>
2022-05-19 16:39:20 -0400

    Update copy and styling

---
+++ src/components/about/About.css.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+/* #655DC6 for purple */
+/* #F5ACB8 for pink */
+/* #1F1F1F for dark gray bg */
+/* #764D25 for brown gradient in footer */
+/* #C59B6C for light brown gradient in footer */
+/* #48ADE5 for blue circle on home page */
+
+export const WhiteBGSection = styled.div`
+  background-color: white;
+`;
+
+export const DarkGreyBGSection = styled.div`
+  background-color: #1F1F1F;
+  color: white;
+`;
+
+export const AboutMain = styled.main`
+  display: flex; 
+  flex-direction: column; 
+  margin: 10%;
+`;
+
+export const PurpleSpan = styled.span`
+  color: #655DC6;
+`;
