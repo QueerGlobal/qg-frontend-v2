f584f5b6cff9a53b5aadf349df65696c9563bcca
Meke Brown <mekesiabrown@gmail.com>
2022-05-28 01:41:31 -0400

    Merge latest

--- src/components/about/About.css.ts
+++ src/components/about/About.css.ts
@@ -4,6 +4,64 @@ import colors from "../../styles/colors";
 /* PRIMARY: #655DC6 for purple */
 /* COTTON_CANDY: #F5ACB8 for pink */
 /* #48ADE5 for blue circle on home page */
+import { Container } from "react-bootstrap";
+
+interface PropsShape {
+	// shape: string;
+	left: string;
+	top: string;
+	width: string;
+	height: string;
+}
+
+export const AboutContainer = styled(Container)`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+export const ColorBlue = styled.div`
+	height: 2595px;
+	width: 100vw;
+	background: #655dc6;
+`;
+
+export const Shape = styled.img<PropsShape>`
+	position: absolute;
+	left: ${({ left }) => left};
+	top: ${({ top }) => top};
+	width: ${({ width }) => width};
+	height: ${({ height }) => height};
+`;
+
+export const MiddleDiv = styled.div`
+	position: absolute;
+	width: 90%;
+	background: #fff;
+	color: #000;
+	top: 30%;
+	border-radius: 7px;
+`;
+
+export const Header = styled.h1`
+	text-transform: uppercase;
+	color: #000;
+`;
+
+export const Paragraph = styled.p`
+	color: #1f1f1f;
+	opacity: 1;
+`;
+
+export const TahuFont = styled.span`
+	color: #655dc6;
+	font-family: "Tahu!", sans-serif;
+	font-size: 61px;
+	font-weight: 500;
+	line-height: 1.2;
+	margin-left: 1.5rem;
+	margin-bottom: 0.5rem;
+	text-transform: lowercase;
+`;
 
 export const WhiteBGSection = styled.div`
   background-color: ${colors.WHITE};
--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,50 +1,62 @@
 import React, { FC } from "react";
-import { AboutMain, WhiteBGSection, PurpleSpan, DarkGreyBGSection, DnDImg } from './About.css';
-import DnDImgSrc from "../../assets/misc/placeholder.jpeg";
+import { 
+	AboutContainer,
+	MiddleDiv,
+	Header,
+	Paragraph,
+	TahuFont,
+	AboutMain, 
+	WhiteBGSection, 
+	PurpleSpan, 
+	DarkGreyBGSection } from './About.css';
+import Background from "./Background";
 
 const About: FC = () => {
 	return (
-		<AboutMain>
-			<WhiteBGSection>
-				<h2>ABOUT US.</h2>
-				<p>We support and prioritize the people who are least represented in an outside of the rainbow umbrella. We want to give the community a safe, inviting space to confidently be themselves, easily find accurate, trusted information, share common experiences, and have the ability to support other LGBTQIA+ people and businesses in their area.</p>
-		
-				<h2>WE ARE <PurpleSpan>united</PurpleSpan>.</h2>
-				<p>We are Black. We are Latinx. We are Asian, Middle Eastern, White, Bi and Multi-Racial. We are tall, short, curvy, plus size, fat. We use chairs, canes, crutches, lifts, and braces.</p>
+		<>
+			<Background />
+			<AboutMain>
+				<WhiteBGSection>
+					<h2>ABOUT US.</h2>
+					<p>We support and prioritize the people who are least represented in an outside of the rainbow umbrella. We want to give the community a safe, inviting space to confidently be themselves, easily find accurate, trusted information, share common experiences, and have the ability to support other LGBTQIA+ people and businesses in their area.</p>
+			
+					<h2>WE ARE <PurpleSpan>united</PurpleSpan>.</h2>
+					<p>We are Black. We are Latinx. We are Asian, Middle Eastern, White, Bi and Multi-Racial. We are tall, short, curvy, plus size, fat. We use chairs, canes, crutches, lifts, and braces.</p>
 
-				<p><strong>And we believe that kind of diversity is what makes Queerness beautiful.</strong></p>
+					<p><strong>And we believe that kind of diversity is what makes Queerness beautiful.</strong></p>
 
-				<p>We strive to be a place where ALL people can talk about the solutions they found to specific crises in their lives, so other people do not have to go through them as well. A place where people do not have to fear outing themselves to coworkers and friends who follow them when discussing physical limitations, needs for legal advice, future medical procedures, or their financial situations. Queer Global is a worldwide masterlist for marginalized peoples in the Queer community.</p>
-		
-				<h2>WE ARE <PurpleSpan>proud</PurpleSpan>.</h2>
-				<p>Queer Global is run by a Black trans/pan/poly/not-so-able-bodied man and a multi-racial pan/poly musician, business owner, stylist and vintage clothing expert.</p>
-		
-				<p>Our goal is to provide a platform that consolidates resources and information for people outside of the Big 5 social media platforms, where it gets buried in posts, behind locked accounts and manipulative algorithms.</p>
+					<p>We strive to be a place where ALL people can talk about the solutions they found to specific crises in their lives, so other people do not have to go through them as well. A place where people do not have to fear outing themselves to coworkers and friends who follow them when discussing physical limitations, needs for legal advice, future medical procedures, or their financial situations. Queer Global is a worldwide masterlist for marginalized peoples in the Queer community.</p>
+			
+					<h2>WE ARE <PurpleSpan>proud</PurpleSpan>.</h2>
+					<p>Queer Global is run by a Black trans/pan/poly/not-so-able-bodied man and a multi-racial pan/poly musician, business owner, stylist and vintage clothing expert.</p>
+			
+					<p>Our goal is to provide a platform that consolidates resources and information for people outside of the Big 5 social media platforms, where it gets buried in posts, behind locked accounts and manipulative algorithms.</p>
 
-				<p>We embraced the transparency of shared community advice and information. With all the resources you need at your fingertips, we hope you can be confident in just being yourself every day.</p>
+					<p>We embraced the transparency of shared community advice and information. With all the resources you need at your fingertips, we hope you can be confident in just being yourself every day.</p>
 
-				<p>We believe providing information is only the beginning & we're just getting started.</p>
+					<p>We believe providing information is only the beginning & we're just getting started.</p>
 
-				<em>Deacon & D'Arcy</em>
-				<DnDImg src={DnDImgSrc} alt="Deacon and D'Arcy" />
-			</WhiteBGSection>
+					<em>Deacon & D'Arcy</em>
+					<div>INSERT PIC</div>
+				</WhiteBGSection>
 
-			<DarkGreyBGSection>
-				<h2>WE ARE <PurpleSpan>family</PurpleSpan>.</h2>
-				<small>Our amazing volunteers!</small>
-				<div>INSERT THUMBNAILS</div>
+				<DarkGreyBGSection>
+					<h2>WE ARE <PurpleSpan>family</PurpleSpan>.</h2>
+					<small>Our amazing volunteers!</small>
+					<div>INSERT THUMBNAILS</div>
 
-				<small>We're all different, but still all the same. Our family is always growing.</small>
-				<button>SEE VOLUNTEER OPPORTUNITIES</button>
-				
-				<small>We want YOU to feel represented and worthy as you are. The people you see on our site are real Queer Global members. Want to join them?</small>
-				<button>SEND US YOUR PHOTO OR ARTWORK</button>
-		
-				<h2>WE ARE HERE FOR <PurpleSpan>you</PurpleSpan>.</h2>
-				<p>We do our best to provide up-to-date, accurate resources and information, but we could always use a helping hand. If you know of any helpful resources, please help us and the rest of our community out and add them.</p>
-				<button>ADD A RESOURCE</button>
-			</DarkGreyBGSection>
-		</AboutMain>
+					<small>We're all different, but still all the same. Our family is always growing.</small>
+					<button>SEE VOLUNTEER OPPORTUNITIES</button>
+					
+					<small>We want YOU to feel represented and worthy as you are. The people you see on our site are real Queer Global members. Want to join them?</small>
+					<button>SEND US YOUR PHOTO OR ARTWORK</button>
+			
+					<h2>WE ARE HERE FOR <PurpleSpan>you</PurpleSpan>.</h2>
+					<p>We do our best to provide up-to-date, accurate resources and information, but we could always use a helping hand. If you know of any helpful resources, please help us and the rest of our community out and add them.</p>
+					<button>ADD A RESOURCE</button>
+				</DarkGreyBGSection>
+			</AboutMain>
+		</>
 	);
 };
 
