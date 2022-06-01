59917968b1653b2dab12a2bc32aecb46868024f0
Meke Brown <mekesiabrown@gmail.com>
2022-06-01 18:49:11 -0400

    Form bottom half of About page bg, with Black svg as a bg img; Add more shapes and temporary filler content

--- src/components/common/Background.css.ts
+++ src/components/common/Background.css.ts
@@ -22,21 +22,129 @@ export const HuggingImg = styled.img<PropsShape>`
 `;
 
 export const BackgroundColor = styled.div`
-	height: 2595px;
-	width: 100vw;
+	width: 100%;
+	height: 1000px;
 	background: ${colors.PRIMARY};
 `;
 
 /* CSS SHAPES */
-export const CottonCandyX = styled.div`
+// A gradient triangle, light pink to pink
+export const GradientPinkTriangle = styled.div`
+	position: absolute;
+	left: 33%;
+	top: 20%;
+	width:85px;
+	height: 70px;
+	background: linear-gradient(#f4a6b7, #f17eae);
+	-webkit-clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
+	clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
+	-webkit-transform: rotate(-62deg);
+	transform: rotate(-62deg);
+`;
+
+// NineWhitePointsStarDiv and SingleWhitePoint create the nine-pointed white star (empty inner circle)
+export const NineWhitePointsStarDiv = styled.div`
+	height: 75px;
+	width: 75px;
+	position: absolute;
+	top: 40%;
+	left: 20%;
+`;
+
+export const SingleWhitePoint = styled.div`
+	height: 20px;
+	width: 3px;
+	background-color: white;
+	position: absolute;
+
+	&.SWPT1 {
+		top: 28px;
+		left: 50px;
+	}
+	
+	&.SWPT2 {
+		top: 31px;
+		left: 38px;
+		-webkit-transform: rotate(-40deg);
+		transform: rotate(-40deg);
+	}
+	
+	&.SWPT3 {
+		top: 42px;
+		left: 31px;
+		-webkit-transform: rotate(-80deg);
+		transform: rotate(-80deg);
+	}
+	
+	&.SWPT4 {
+		top: 56px;
+		left: 34px;
+		-webkit-transform: rotate(-120deg);
+		transform: rotate(-120deg);
+	}
+	
+	&.SWPT5 {
+		top: 64px;
+		left: 44px;
+		-webkit-transform: rotate(-160deg);
+		transform: rotate(-160deg);
+	}
+	
+	&.SWPT6 {
+		top: 31px;
+		left: 61px;
+		-webkit-transform: rotate(40deg);
+		transform: rotate(40deg);
+	}
+	
+	&.SWPT7 {
+		top: 42px;
+		left: 68px;
+		-webkit-transform: rotate(80deg);
+		transform: rotate(80deg);
+	}
+	
+	&.SWPT8 {
+		top: 55px;
+		left: 66px;
+		-webkit-transform: rotate(120deg);
+		transform: rotate(120deg);
+	}
+	
+	&.SWPT9 {
+		top: 63px;
+		left: 58px;
+		-webkit-transform: rotate(154deg);
+		transform: rotate(154deg);
+	}
+`;
+
+export const SVGDiv = styled.div`
+	-webkit-transform: rotate(-35deg);
+	transform: rotate(-35deg);
+`;
+
+export const TinyCottonCandyX = styled.div`
 	&:before {
 		content: "+";
 		position: absolute;
 		color: ${colors.COTTON_CANDY};
 		font-weight: bold;
-		font-size: 80px;
-		right: 359px;
-		top: 36px;
+		left: 109px;
+		top: 155px;
+		font-size: 15px;
+	}
+`;
+
+export const LargeCottonCandyX = styled.div`
+	&:before {
+		content: "+";
+		position: absolute;
+		color: ${colors.COTTON_CANDY};
+		font-weight: bold;
+		right: 109px;
+		top: 106px;
+		font-size: 40px;
 	}
 `;
 
@@ -44,28 +152,80 @@ export const WhiteX = styled.div`
 	&:before {
 		content: "+";
 		position: absolute;
-		color: ${colors.WHITE};
+		color: #FFFFFF;
 		font-weight: bold;
-		font-size: 80px;
-		right: 259px;
-		top: 136px;
+		font-size: 45px;
+		right: 275px;
+		top: 186px;
 		-webkit-transform: rotate(45deg);
-		transform: rotate(45deg);
+		-webkit-transform: rotate(45deg);
+		-ms-transform: rotate(45deg);
 	}
 `;
 
-// Width originally 420			624
 export const BlackSolidHex = styled.div`
 	background-color: ${colors.BLACK};
 	width: 520px;
 	height: 520px;
 	position: absolute;
 	left: 515px;
-	top: 250px;
+	top: 100px;
 	-webkit-clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
 	clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
 	-webkit-transform: rotate(15deg);
 	transform: rotate(15deg);
+`;
+
+				// Unfinished
+export const BorderedHex = styled.div`
+	display: block;
+	margin: 0 auto;
+	position: relative;
+	width: 320px;
+	height: 277.12px; /* width * 0.866 */
+	background: red;
+	box-sizing: border-box;
+	-webkit-clip-path: polygon(0% 50%, 25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%);
+	-moz-clip-path: polygon(0% 50%, 25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%);
+
+& .hex-background {
+	position: absolute;
+	background-color: orange; /*color of the main-background*/
+	top: 2px; /* equal to border thickness */
+	left: 2px; /* equal to border thickness */
+	width: 316px; /* container height - (border thickness * 2) */
+	height: 273.12px; /* container height - (border thickness * 2) */
+	-webkit-clip-path: polygon(0% 50%, 25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%);
+	-moz-clip-path: polygon(0% 50%, 25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%);
+  }
+`;
+
+export const WhiteXTwo = styled.div`
+	&:before {
+		content: "+";
+		position: absolute;
+		color: ${colors.WHITE};
+		font-weight: bold;
+		font-size: 40px;
+		left: 105px;
+		top: 150px;
+		-webkit-transform: rotate(45deg);
+		transform: rotate(45deg);
+	}
+`;
+
+export const BlackX = styled.div`
+	&:before {
+		content: "+";
+		position: absolute;
+		color: ${colors.BLACK};
+		font-weight: bold;
+		font-size: 40px;
+		left: 59px;
+		top: 100px;
+		-webkit-transform: rotate(45deg);
+		transform: rotate(45deg);
+	}
 `;
 
 export const CottonCandySolidHex = styled.div`
@@ -126,10 +286,10 @@ export const NineWhiteSpikes = styled.div`
 
 export const WhiteTriangle = styled.div`
 	position: absolute;
-	right: 15px;
-	top: 50px;
-	width:100px;
-	height: 100px;
+	right: 25%;
+	top: 20%;
+	width: 50px;
+	height: 50px;
 	background: ${colors.WHITE};
 	-webkit-clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
 	clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
@@ -143,5 +303,58 @@ export const CircleShape = styled.div`
 	width: 20px;
 	-webkit-clip-path: circle(50%);
 	clip-path: circle(50%);
+`;
+
+export const CottonCandySemiOpaqueRect = styled.div`
+	background-color: ${colors.COTTON_CANDY};
+	width: 300px;
+	height: 150px;
+	opacity: 0.5;
+`;
+
+export const BlackBG = styled.div`
+	position: absolute;
+	top: 225%;
+	height: 400px;
+	width: 10%;
+	background-color: ${colors.BAHAMAS_HOVER};
+`;
+
+export const Cont = styled.div`
+	background-image:
+         url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 800 1200'%3E%3Cpath fill='black' fill-opacity='1' d='M 0 200 L 150 50 C 150 50 200 0 300 50 L 800 300 L 800 1200 L 0 1200 L 0 200'%3E%3C/path%3E%3C/svg%3E");
+	background-repeat: no-repeat;
+	background-size: cover;
+	width: 100%; 
+	height: 1200px;
+	padding: 25% 0 0 25%;
+	margin: auto; 
+	color: white;
+`;
+
+export const GridOfPics = styled.div`
+	display: flex; 
+	justify-content: space-evenly;
+	width: 40%;
+`;
+
+export const Cols = styled.div`
+	flex: 1;
+`;
+
+export const Blue = styled.div` 
+	border: 1px solid blue;
+`;	
+
+export const Yellow = styled.div`
+	border: 1px solid yellow;
+`;	
+
+export const Red = styled.div`
+	border: 1px solid red;
+`;	
+
+export const Pink = styled.div`
+border: 1px solid pink;
 `;
 /* END OF CSS SHAPES */
--- src/components/common/Background.tsx
+++ src/components/common/Background.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
-import { BackgroundColor, HuggingImg, CottonCandyX, WhiteX, BlackSolidHex, CottonCandySolidHex, WhiteTriangle, CircleShape, NineWhiteSpikes } from "./Background.css";
+import { BackgroundColor, TinyCottonCandyX, LargeCottonCandyX, HuggingImg, WhiteX, WhiteXTwo, BlackSolidHex, CottonCandySolidHex, WhiteTriangle, GradientPinkTriangle, CircleShape, NineWhiteSpikes, BlackBG, BlackX, CottonCandySemiOpaqueRect, SVGDiv, Cont, Cols, Blue, Red, Pink, Yellow, GridOfPics, NineWhitePointsStarDiv, SingleWhitePoint, BorderedHex } from "./Background.css";
 import hugging from "../../assets/misc/placeholder.jpeg";
+import colors from "../../styles/colors";
 
 /**
  * Testing out pure CSS shapes for background decoration
@@ -12,34 +13,102 @@ import hugging from "../../assets/misc/placeholder.jpeg";
  */
 const Background: FC = () => {
 	return (
-		<BackgroundColor>
-			<HuggingImg
-				// shape={hugging}
-				left="25%"
-				top="70px"
-				width="200px"
-				height="300px"
-				src={hugging}
-			/>
-			
-			<CottonCandyX />
+			<BackgroundColor className="bgColor">
+				<GradientPinkTriangle /> 
 
-			<WhiteX />
-			
-			<BlackSolidHex />
+				<NineWhitePointsStarDiv>
+					<SingleWhitePoint className="SWPT1" />
+					<SingleWhitePoint className="SWPT2" />
+					<SingleWhitePoint className="SWPT3" />
+					<SingleWhitePoint className="SWPT4" />
+					<SingleWhitePoint className="SWPT5" />
+					<SingleWhitePoint className="SWPT6" />
+					<SingleWhitePoint className="SWPT7" />
+					<SingleWhitePoint className="SWPT8" />
+					<SingleWhitePoint className="SWPT9" />
+				</NineWhitePointsStarDiv>
 
-			<CottonCandySolidHex className="ccsh1" />
-			<CottonCandySolidHex className="ccsh2" />
-			<CottonCandySolidHex className="ccsh3" />
+				<SVGDiv>
+					<svg viewBox="0 0 1000 1000" version="1.1">
+						<path d="M 740 230 L 780 190 L 800 250 L 740 230" stroke="#FFFFFF" stroke-width="2" fill="none"></path>
+					</svg>
+				</SVGDiv>
 
-			<WhiteTriangle />
+				<TinyCottonCandyX />
 
-			<CircleShape />
+				<LargeCottonCandyX />
+				
+				<WhiteX />
+				
+				<BlackSolidHex />
+				
+				{/** Unfinished */}
 
-			<NineWhiteSpikes className="nws1" />
-			<NineWhiteSpikes className="nws2" />
-			<NineWhiteSpikes className="nws3" />
-		</BackgroundColor>
+				{/* <WhiteXTwo />
+
+				<CottonCandySolidHex className="ccsh1" />
+				<CottonCandySolidHex className="ccsh2" />
+				<CottonCandySolidHex className="ccsh3" />
+
+				<CircleShape />
+
+				<NineWhiteSpikes className="nws1" />
+				<NineWhiteSpikes className="nws2" />
+				<NineWhiteSpikes className="nws3" />
+
+				<CottonCandySemiOpaqueRect />
+
+				<BlackX />
+
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas erat imperdiet sed euismod nisi porta. Senectus et netus et malesuada. Id diam vel quam elementum pulvinar. Elementum eu facilisis sed odio morbi quis commodo odio aenean. Bibendum est ultricies integer quis auctor elit. Vel quam elementum pulvinar etiam non. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Nibh ipsum consequat nisl vel pretium lectus quam. Vitae auctor eu augue ut lectus arcu. Lectus quam id leo in vitae. Aliquam etiam erat velit scelerisque in. Duis tristique sollicitudin nibh sit amet. Pulvinar sapien et ligula ullamcorper malesuada proin libero.
+				</p>
+				<p>
+					Purus sit amet luctus venenatis lectus magna fringilla urna. Arcu non odio euismod lacinia at quis risus sed vulputate. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo. Bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida. Cras adipiscing enim eu turpis egestas pretium aenean. Nibh praesent tristique magna sit amet. Porttitor rhoncus dolor purus non enim praesent elementum facilisis. Semper auctor neque vitae tempus quam pellentesque nec. Libero justo laoreet sit amet cursus sit amet. Condimentum vitae sapien pellentesque habitant. Cras sed felis eget velit aliquet sagittis. Vivamus at augue eget arcu dictum varius duis at. Tellus cras adipiscing enim eu. Amet est placerat in egestas. Mauris vitae ultricies leo integer malesuada nunc vel risus commodo. Enim blandit volutpat maecenas volutpat blandit. Eros in cursus turpis massa tincidunt. Magna fringilla urna porttitor rhoncus dolor. Egestas diam in arcu cursus euismod quis viverra nibh.
+				</p>
+				<Cont>
+					<strong>WE ARE <span>family</span>.</strong>
+					<p><small>Our amazing volunteers!</small></p>
+					<GridOfPics>
+						<Cols>
+							<Blue>Placeholder</Blue>
+							<Blue>Placeholder</Blue>
+							<Blue>Placeholder</Blue>
+							<Blue>Placeholder</Blue>
+						</Cols>
+						<Cols>
+							<Red>Placeholder</Red>
+							<Red>Placeholder</Red>
+							<Red>Placeholder</Red>
+							<Red>Placeholder</Red>
+						</Cols>
+						<Cols>
+							<Pink>Placeholder</Pink>
+							<Pink>Placeholder</Pink>
+							<Pink>Placeholder</Pink>
+							<Pink>Placeholder</Pink>
+						</Cols>
+						<Cols>
+							<Yellow>Placeholder</Yellow>
+							<Yellow>Placeholder</Yellow>
+							<Yellow>Placeholder</Yellow>
+							<Yellow>Placeholder</Yellow>
+						</Cols>
+						<Cols>
+							<Pink>Placeholder</Pink>
+							<Pink>Placeholder</Pink>
+							<Pink>Placeholder</Pink>
+							<Pink>Placeholder</Pink>
+						</Cols>
+						<Cols>
+							<Yellow>Placeholder</Yellow>
+							<Yellow>Placeholder</Yellow>
+							<Yellow>Placeholder</Yellow>
+							<Yellow>Placeholder</Yellow>
+						</Cols>
+					</GridOfPics>
+				</Cont> */}
+			</BackgroundColor>
 	);
 };
 
