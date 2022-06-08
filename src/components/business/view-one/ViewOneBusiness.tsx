ef2525de331785961f141e808909379ea5adcdb7
Meke Brown <mekesiabrown@gmail.com>
2022-06-08 15:40:50 -0400

    Add TODO comments

--- src/components/business/view-one/ViewOneBusiness.tsx
+++ src/components/business/view-one/ViewOneBusiness.tsx
@@ -1,6 +1,11 @@
 // https://xd.adobe.com/view/6f6af599-8eb6-46b5-8731-e4c09ddd587e-3a39/screen/9d59dc18-80d5-4e68-94f7-7bb7738cbf76/
 import React, { FC } from 'react';
 
+/**
+ * A business will be rated by logged-in? users so the rating can be set here
+ * 
+ * @returns 
+ */
 const ViewOneBusiness: FC = () => {
     return <h1>Hello</h1>;
 }
--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,5 +1,10 @@
 import React, { FC } from "react";
 
+/**
+ * Remember: A user likes a business so the business likes are saved in their user data
+ * 
+ * @returns 
+ */
 const Profile: FC = () => {
 	return (
 		<div>
