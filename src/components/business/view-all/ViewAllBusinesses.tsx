e031139c063f363ae0847b51d15a07994226892b
Meke Brown <mekesiabrown@gmail.com>
2022-06-08 15:40:20 -0400

    Add component: Form business card/ratings/likes

--- src/components/business/view-all/ViewAllBusinesses.tsx
+++ src/components/business/view-all/ViewAllBusinesses.tsx
@@ -1,38 +1,17 @@
 // https://xd.adobe.com/view/6f6af599-8eb6-46b5-8731-e4c09ddd587e-3a39/screen/9a020f3a-4b71-42e5-8ff4-2c93f15ddb28/
 import React, { FC, useState } from 'react';
-import placeholder from "../../assets/misc/placeholder.jpeg";
+import Business from "../data/business-data";
+import { BusinessesFilter } from "./ViewAllBusinesses.css";
+
 
 const ViewAllBusinesses: FC = () => {
-    const [loadMoreBusinesses, setLoadMoreBusinesses] = useState(false);
-    const businesses = [
-        {
-            key: 1, 
-            img: {placeholder}, 
-            cat: "CATEGORY", 
-            name: "Business Name", 
-            desc: "This is where the description goes for business. You can add additional, conference schedules, and more here.", 
-            like: 1, 
-            location: "Location Name", 
-            stars: 5
-        },
-        {
-            key: 2, 
-            img: {placeholder}, 
-            cat: "CATEGORY", 
-            name: "Business Name", 
-            desc: "This is where the description goes for business. You can add additional, conference schedules, and more here.", 
-            like: 0, 
-            location: "Location Name", 
-            stars: 5
-        }
-    ];
-
+    const [loadMoreBusinesses, setLoadMoreBusinesses] = useState(false); // Businesses will be loaded from subsequent xhr calls
 
     return (
         <>
             <h1>FIND BUSINESSES</h1>
             <section>
-                <div>
+                <BusinessesFilter>
                     <div>
                         <label htmlFor="listing">FIND LISTING</label>
                         <input type="text" name="listing" id="listing" placeholder="Cape Verdean Food" />
@@ -54,18 +33,9 @@ const ViewAllBusinesses: FC = () => {
                         <a href="/"><small>CLEAR ALL</small></a>
                         <button>APPLY</button>
                     </div>
-                </div>
-                {businesses.map((key, img, cat, name, desc, like, location, stars) => {
-                    return <div id={key} className={key}>
-                        <img src={img} alt={name} />
-                        <p>{cat}</p>
-                        <p>{name}</p>
-                        <p>{desc}</p>
-                        <div>{like}</div>
-                        <hr />
-                        <p><span>{location}</span><span>{stars}</span></p>
-                    </div>;
-                })}
+                </BusinessesFilter>
+                {/** Where to map the businesses */}
+                <Business key="string" img="string" cat="string" name="string" desc="string" loc="string" rating={1} />
             </section>
             <button>SEE MORE BUSINESSES</button>
         </>
