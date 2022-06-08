5c479ed22f1cd512b779d44f40c15c3ef29d9440
Meke Brown <mekesiabrown@gmail.com>
2022-06-08 15:37:02 -0400

    Start styling ts

---
+++ src/components/business/view-all/ViewAllBusinesses.css.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import colors from '../../../styles/colors';
+
+export const BusinessesFilter = styled.div`
+	display: flex;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+`;
