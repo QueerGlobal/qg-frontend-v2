0682074b6421c321ef2c5fcbb62500e1971be296
Meke Brown <mekesiabrown@gmail.com>
2022-06-09 12:34:24 -0400

    Notes for future code

--- src/components/business/add-a-business/BusinessBasicInfo.tsx
+++ src/components/business/add-a-business/BusinessBasicInfo.tsx
@@ -1,7 +1,24 @@
 // https://xd.adobe.com/view/6f6af599-8eb6-46b5-8731-e4c09ddd587e-3a39/screen/2cffa014-0880-4777-b43e-a1118dad69f8/
 import React, { FC } from 'react';
 
+/**
+ * Look into html-escaper at node_modules\html-escaper\README.md
+ * or he at node_modules\he\README.md
+ * and body-parser node_modules\body-parser\README.md
+ * 
+ * @returns 
+ */
 const BusinessBasicInfo: FC = () => {
+    // html-entities example, from node_modules\html-entities\README.md    
+    const Entities = require('html-entities').AllHtmlEntities;
+
+    const entities = new Entities();
+
+    console.log(entities.encode('<>"&©®∆')); // &lt;&gt;&quot;&amp;&copy;&reg;∆
+    console.log(entities.encodeNonUTF('<>"&©®∆')); // &lt;&gt;&quot;&amp;&copy;&reg;&#8710;
+    console.log(entities.encodeNonASCII('<>"&©®∆')); // <>"&©®&#8710;
+    console.log(entities.decode('&lt;&gt;&quot;&amp;&copy;&reg;')); // <>"&©®
+
     return <h1>Hello</h1>;
 };
 
--- src/components/business/add-a-business/BusinessSubmit.tsx
+++ src/components/business/add-a-business/BusinessSubmit.tsx
@@ -1,7 +1,15 @@
 // https://xd.adobe.com/view/6f6af599-8eb6-46b5-8731-e4c09ddd587e-3a39/screen/5fbdd85b-0b5a-4eba-9d0a-1c4f62e36005/
+import { nanoid } from 'nanoid'
 import React, { FC } from 'react';
 
+/**
+ * Add a unique id to the business with nanoid
+ * @returns 
+ */
 const BusinessSubmit: FC = () => {
+    let businessName = "Temp Name";
+    let id = businessName.replace(/\s/g, "") + "-" + nanoid(6); // Example of result would be "TempName-I_Fa-V"
+
     return <h1>Hello</h1>;
 };
 
