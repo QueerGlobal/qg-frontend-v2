99356e631a345da52460f3a52c07a8b7438e7c34
Meke Brown <mekesiabrown@gmail.com>
2022-06-10 18:24:38 -0400

    More building out of all businesses directory page

--- src/components/business/view-all/BusinessCard.tsx
+++ src/components/business/view-all/BusinessCard.tsx
@@ -1,10 +1,11 @@
 // https://xd.adobe.com/view/6f6af599-8eb6-46b5-8731-e4c09ddd587e-3a39/screen/9a020f3a-4b71-42e5-8ff4-2c93f15ddb28/
-import { BusinessCardContainer } from "./BusinessCard.css";
+import { BusinessCardContainer, Thumbnail, GoldStar, EmptyStar } from "./BusinessCard.css";
 
 /**
  * On the wireframe "Location Name" is listed. Is this to be the full address of the business?
  */
 interface BusinessCardInfo {
+    key: string, 
     id: string, 
     thumbnail: string, 
     name: string, 
@@ -15,16 +16,60 @@ interface BusinessCardInfo {
     liked: boolean
 };
 
+const ReviewStars = (rating: number): any => {
+    let ratingStart = 0;
+    let allStars: any;
+
+    do {
+        allStars += GoldStar;
+        ratingStart++;
+    } while (ratingStart < rating);
+
+    return allStars;
+};
+
+const RemainingStars = (rating: number): any => {
+    let restStart = 0;
+    let allStars: any;
+
+    do {
+        allStars += EmptyStar;
+        restStart++;
+    } while (restStart < 5 - rating);
+
+    return allStars;
+};
+
 const BusinessCard = (props: BusinessCardInfo): any => {
     return <BusinessCardContainer>
-        <img src={props.thumbnail} alt={props.name} />
+        <Thumbnail src={props.thumbnail} alt={props.name} />
         <p>{props.cat}</p>
         <p>{props.name}</p>
         <p>{props.desc}</p>
         <p>{props.liked}</p>
         <hr />
-        <p><span>{props.loc}</span><span>{props.rating}</span></p>
+        <p>
+            <span>{props.loc}</span>
+            <GoldStar /><EmptyStar />{/* TODO - How to generate the stars?!*/}
+        </p>
     </BusinessCardContainer>;
 }
 
+const GenerateCards = (items: Array<any>): any => {
+    return items.map((item, index) => (
+        <BusinessCard   
+            key={item.key} 
+            id={item.id} 
+            thumbnail={item.thumbnail} 
+            cat={item.cat}
+            name={item.name}
+            desc={item.desc}
+            loc={item.loc}
+            rating={item.rating}
+            liked={item.liked}
+        />
+      ));
+};
+
 export default BusinessCard;
+export {GenerateCards};
--- src/components/business/view-all/ViewAllBusinesses.tsx
+++ src/components/business/view-all/ViewAllBusinesses.tsx
@@ -1,11 +1,250 @@
 // https://xd.adobe.com/view/6f6af599-8eb6-46b5-8731-e4c09ddd587e-3a39/screen/9a020f3a-4b71-42e5-8ff4-2c93f15ddb28/
 import React, { FC, useState } from 'react';
-import BusinessCard from "./BusinessCard";
-import { BusinessesFilter } from "./ViewAllBusinesses.css";
+import { GenerateCards } from "./BusinessCard";
+import { BusinessesFilter, AllBusinesses } from "./ViewAllBusinesses.css";
 
-
+/**
+ * Maximum cards in a row: 3
+ * Maximum cards in a page load: 21 (3 cards x 7 rows)
+ * 
+ * @returns {Node} ViewAllBusinesses
+ */
 const ViewAllBusinesses: FC = () => {
     const [loadMoreBusinesses, setLoadMoreBusinesses] = useState(false); // Businesses will be loaded from subsequent xhr calls
+
+    const items = [
+        {
+            id: "1-a", 
+            key: "1-a-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "2-b", 
+            key: "2-b-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "3-c", 
+            key: "3-c-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "4-a", 
+            key: "4-a-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "5-b", 
+            key: "5-b-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "6-c", 
+            key: "6-c-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "7-a", 
+            key: "7-a-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "8-b", 
+            key: "8-b-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "9-c", 
+            key: "9-c-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "10-a", 
+            key: "10-a-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "11-b", 
+            key: "11-b-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "12-c", 
+            key: "12-c-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "13-a", 
+            key: "13-a-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "14-b", 
+            key: "14-b-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "15-c", 
+            key: "15-c-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "16-a", 
+            key: "16-a-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "17-b", 
+            key: "17-b-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "18-c", 
+            key: "18-c-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "19-a", 
+            key: "19-a-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "20-b", 
+            key: "20-b-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        },
+        {
+            id: "21-c", 
+            key: "21-c-key", 
+            thumbnail: "placeholder", 
+            cat: "cat", 
+            name: "name", 
+            desc: "desc", 
+            loc: "loc", 
+            rating: 1, 
+            liked: true, 
+        }
+    ];
 
     return (
         <>
@@ -36,11 +275,15 @@ const ViewAllBusinesses: FC = () => {
                     </div>
                 </BusinessesFilter>
 
-                {/** Where to map the businesses. Info will be populated from an xhr value from useEffect */}
-                <BusinessCard id="unique-id-generated" thumbnail="string" cat="string" name="string" desc="string" loc="string" rating={1} liked={false} />
+                <AllBusinesses>
+                {
+                    /** Where to map the businesses. Info will be populated from an xhr value from useEffect */
+                    GenerateCards(items)
+                }
+                </AllBusinesses>
             </section>
 
-            <button>SEE MORE BUSINESSES</button>
+            <button type="button">SEE MORE BUSINESSES</button>
         </>
     );
 };
