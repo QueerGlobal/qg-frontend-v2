770101d4e2cb20f6bc2c2ce7ba8176ad60dc122a
Mekesia Brown <mekesiabrown@gmail.com>
2022-06-11 11:37:10 -0400

    Add uniformity to colors; Colors need one source (#18)

--- src/components/navbar/Navbar.css.ts
+++ src/components/navbar/Navbar.css.ts
@@ -1,56 +1,57 @@
 import styled from "styled-components";
 import { Nav, NavDropdown, Container } from "react-bootstrap";
+import colors from "../../styles/colors";
 
 export const NavButtonContainer = styled.div`
 	position: fixed;
 	width: 100%; /* TODO add break points for desktop screen */
 	height: 50px;
-	background: #655dc6;
+	background: ${colors.PRIMARY};
 	box-shadow: 0px 1px 10px rgba(0, 0, 0, 0.25);
 	z-index: 1050;
 `;
 export const MenuButton = styled.button`
-	background: #655dc6;
+	background: ${colors.PRIMARY};
 	border: transparent;
 	font-size: 25px;
 	margin: auto 10px;
 `;
 export const CanvasNavbarContainer = styled.div`
 	width: 50vw;
-	background: #655dc6;
-	color: #ffffff;
+	background: ${colors.PRIMARY};
+	color: ${colors.WHITE};
 	padding-top: 60px;
 `;
 
 export const NavLink = styled(Nav.Link)`
-	color: #ffffff;
+	color: ${colors.WHITE};
 	font-size: 18px;
 	letter-spacing: 34;
 
 	&:focus,
 	&:active {
-		color: #ffffff;
+		color: ${colors.WHITE};
 	}
 `;
 
 export const NavDropdownItem = styled(NavDropdown.Item)`
-	color: #ffffff;
+	color: ${colors.WHITE};
 	font-size: 18px;
 	letter-spacing: 34;
 	padding: 0.5rem 0;
 
 	&:focus,
 	&:active {
-		color: #ffffff;
+		color: ${colors.WHITE};
 	}
 
 	&:before {
 		content: "";
-		background: #fff;
+		background: ${colors.WHITE};
 		position: absolute;
 		top: 0;
 		left: 0;
-		border: 1px solid #fff;
+		border: 1px solid ${colors.WHITE};
 		width: 85%;
 	}
 `;
