fa97f437f590cfb8fd960c816d031720f4c2408a
Meke Brown <mekesiabrown@gmail.com>
2022-06-11 11:30:37 -0400

    Create container for single business view page

---
+++ src/components/business/view-one/ViewOneBusiness.css.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const ViewOneBusinessContainer = styled.main`
+    display: flex; 
+    flex-direction: column; 
+    margin: 10%;
+    position: absolute;
+    top: 200px;
+`;
+
+export const BusinessesFilter = styled.div`
+	display: flex;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    width: 60%;
+`;
+
+export const AllBusinesses = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    width: 60%;
+`;
--- src/components/business/view-one/ViewOneBusiness.tsx
+++ src/components/business/view-one/ViewOneBusiness.tsx
@@ -1,5 +1,6 @@
 // https://xd.adobe.com/view/6f6af599-8eb6-46b5-8731-e4c09ddd587e-3a39/screen/9d59dc18-80d5-4e68-94f7-7bb7738cbf76/
 import React, { FC } from 'react';
+import { ViewOneBusinessContainer } from './ViewOneBusiness.css';
 
 /**
  * A business will be rated by logged-in? users so the rating can be set here
@@ -7,7 +8,9 @@ import React, { FC } from 'react';
  * @returns 
  */
 const ViewOneBusiness: FC = () => {
-    return <h1>Hello</h1>;
+    return <ViewOneBusinessContainer>
+
+    </ViewOneBusinessContainer>
 }
 
 export default ViewOneBusiness;
