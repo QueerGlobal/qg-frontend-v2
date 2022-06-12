a33ef30483fce5dd931706ab35e270c824bf7185
Meke Brown <mekesiabrown@gmail.com>
2022-06-12 14:46:01 -0400

    Start on bg decorations

---
+++ src/components/business/view-all/view-all-background/ViewAllBackground.css.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import colors from '../../../../styles/colors';
+
+/**
+ * Background decoration:
+ * Rotated yellow +
+ * white 9-pointed star
+ * 18-slash black deco
+ * 18-slash light blue deco
+ * Two white +
+ * Two black +
+ * yellow 9-pointed star
+ * Dashed black rect
+ * Two solid light blue rect
+ * Black x
+ * White border rect
+ * Yellow x
+ * Four black ^s in a column
+ * One semi-opaque light blue short rect
+ */
