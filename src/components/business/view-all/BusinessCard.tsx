007972155721f12dfd20ddb21f04148df58a9fc8
Meke Brown <mekesiabrown@gmail.com>
2022-06-13 17:01:37 -0400

    Update business data fields; Move interface to another file

--- src/components/business/view-all/BusinessCard.tsx
+++ src/components/business/view-all/BusinessCard.tsx
@@ -1,20 +1,11 @@
 // https://xd.adobe.com/view/6f6af599-8eb6-46b5-8731-e4c09ddd587e-3a39/screen/9a020f3a-4b71-42e5-8ff4-2c93f15ddb28/
 import { BusinessCardContainer, Thumbnail, GoldStar, EmptyStar } from "./BusinessCard.css";
+import businessInfo from '../business-interfaces';
 
 /**
  * On the wireframe "Location Name" is listed. Is this to be the full address of the business?
  */
-interface BusinessCardInfo {
-    key: string, 
-    id: string, 
-    thumbnail: string, 
-    name: string, 
-    desc: string, 
-    loc: string, 
-    rating: number,
-    cat: string,
-    liked: boolean
-};
+
 
 const ReviewStars = (rating: number): any => {
     let ratingStart = 0;
@@ -40,7 +31,7 @@ const RemainingStars = (rating: number): any => {
     return allStars;
 };
 
-const BusinessCard = (props: BusinessCardInfo): any => {
+const BusinessCard = (props: businessInfo): any => {
     return <BusinessCardContainer>
         <Thumbnail src={props.thumbnail} alt={props.name} />
         <p>{props.cat}</p>
@@ -49,7 +40,7 @@ const BusinessCard = (props: BusinessCardInfo): any => {
         <p>{props.liked}</p>
         <hr />
         <p>
-            <span>{props.loc}</span>
+            <span>{props.addr1}</span>
             <GoldStar /><EmptyStar />{/* TODO - How to generate the stars?!*/}
         </p>
     </BusinessCardContainer>;
@@ -58,13 +49,13 @@ const BusinessCard = (props: BusinessCardInfo): any => {
 const GenerateCards = (items: Array<any>): any => {
     return items.map((item, index) => (
         <BusinessCard   
-            key={item.key} 
+            key={item.id} 
             id={item.id} 
             thumbnail={item.thumbnail} 
             cat={item.cat}
             name={item.name}
             desc={item.desc}
-            loc={item.loc}
+            addr1={item.addr1}
             rating={item.rating}
             liked={item.liked}
         />
