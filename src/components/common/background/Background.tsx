f1d9c49efcfe80730e35906f6060e1ced0c6ac9e
Meke Brown <mekesiabrown@gmail.com>
2022-07-04 10:58:49 -0400

    refactor: remove any page copy and content content from this purely visuals component

--- src/components/common/background/Background.tsx
+++ src/components/common/background/Background.tsx
@@ -65,56 +65,7 @@ const Background: FC = () => {
 
 				<CottonCandySemiOpaqueRect />
 
-				<BlackX />
-
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas erat imperdiet sed euismod nisi porta. Senectus et netus et malesuada. Id diam vel quam elementum pulvinar. Elementum eu facilisis sed odio morbi quis commodo odio aenean. Bibendum est ultricies integer quis auctor elit. Vel quam elementum pulvinar etiam non. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Nibh ipsum consequat nisl vel pretium lectus quam. Vitae auctor eu augue ut lectus arcu. Lectus quam id leo in vitae. Aliquam etiam erat velit scelerisque in. Duis tristique sollicitudin nibh sit amet. Pulvinar sapien et ligula ullamcorper malesuada proin libero.
-				</p>
-				<p>
-					Purus sit amet luctus venenatis lectus magna fringilla urna. Arcu non odio euismod lacinia at quis risus sed vulputate. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo. Bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida. Cras adipiscing enim eu turpis egestas pretium aenean. Nibh praesent tristique magna sit amet. Porttitor rhoncus dolor purus non enim praesent elementum facilisis. Semper auctor neque vitae tempus quam pellentesque nec. Libero justo laoreet sit amet cursus sit amet. Condimentum vitae sapien pellentesque habitant. Cras sed felis eget velit aliquet sagittis. Vivamus at augue eget arcu dictum varius duis at. Tellus cras adipiscing enim eu. Amet est placerat in egestas. Mauris vitae ultricies leo integer malesuada nunc vel risus commodo. Enim blandit volutpat maecenas volutpat blandit. Eros in cursus turpis massa tincidunt. Magna fringilla urna porttitor rhoncus dolor. Egestas diam in arcu cursus euismod quis viverra nibh.
-				</p>
-				<Cont>
-					<strong>WE ARE <span>family</span>.</strong>
-					<p><small>Our amazing volunteers!</small></p>
-					<GridOfPics>
-						<Cols>
-							<Blue>Placeholder</Blue>
-							<Blue>Placeholder</Blue>
-							<Blue>Placeholder</Blue>
-							<Blue>Placeholder</Blue>
-						</Cols>
-						<Cols>
-							<Red>Placeholder</Red>
-							<Red>Placeholder</Red>
-							<Red>Placeholder</Red>
-							<Red>Placeholder</Red>
-						</Cols>
-						<Cols>
-							<Pink>Placeholder</Pink>
-							<Pink>Placeholder</Pink>
-							<Pink>Placeholder</Pink>
-							<Pink>Placeholder</Pink>
-						</Cols>
-						<Cols>
-							<Yellow>Placeholder</Yellow>
-							<Yellow>Placeholder</Yellow>
-							<Yellow>Placeholder</Yellow>
-							<Yellow>Placeholder</Yellow>
-						</Cols>
-						<Cols>
-							<Pink>Placeholder</Pink>
-							<Pink>Placeholder</Pink>
-							<Pink>Placeholder</Pink>
-							<Pink>Placeholder</Pink>
-						</Cols>
-						<Cols>
-							<Yellow>Placeholder</Yellow>
-							<Yellow>Placeholder</Yellow>
-							<Yellow>Placeholder</Yellow>
-							<Yellow>Placeholder</Yellow>
-						</Cols>
-					</GridOfPics>
-				</Cont> */}
+				<BlackX /> */}
 			</BackgroundColor>
 	);
 };
