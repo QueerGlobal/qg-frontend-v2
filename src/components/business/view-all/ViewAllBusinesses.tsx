eaa338367def2658c13d26ba1441d54197700393
Meke Brown <mekesiabrown@gmail.com>
2022-07-11 06:59:57 -0400

    refactor: add initial card count, business loader fcn

--- src/components/business/view-all/ViewAllBusinesses.tsx
+++ src/components/business/view-all/ViewAllBusinesses.tsx
@@ -1,5 +1,5 @@
 // https://xd.adobe.com/view/6f6af599-8eb6-46b5-8731-e4c09ddd587e-3a39/screen/9a020f3a-4b71-42e5-8ff4-2c93f15ddb28/
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { GenerateCards } from "./BusinessCard";
 import { ViewAllBusinessesContainer, BusinessesFilter, AllBusinesses } from "./ViewAllBusinesses.css";
 import { exampleBusiness } from "../business-interfaces";
@@ -12,60 +12,63 @@ import { exampleBusiness } from "../business-interfaces";
  */
 const ViewAllBusinesses: FC = () => {
     const [allBusinesses, setAllBusinesses] = useState<{}[]>([]);
-    const [loadMoreBusinesses, setLoadMoreBusinesses] = useState(false); // Businesses will be loaded from subsequent xhr calls
+    const [count, setCount] = useState(0);
 
-    const generateBusinesses = (): void => {
+    const retrieveBusinesses = (): void => {};
+
+    const tempBusinessGenerator = (num = 22) => {
         let exampleItems: Array<{}> = [];
-    
         let counter: number = 1;
-            exampleItems.push(exampleBusiness);
+
         do {
-    
-        } while (counter < 22);
+            exampleItems.push(exampleBusiness);
+            counter++;
+        } while (counter < num);
 
         setAllBusinesses(exampleItems);
-    }
+    };
 
-    generateBusinesses();
+    useEffect(() => {
+        tempBusinessGenerator();
+
+        setCount(22);
+    }, []);
 
     return (
         <ViewAllBusinessesContainer>
             <h1>FIND BUSINESSES</h1>
             
             <section>
-                <BusinessesFilter>
-                    <div>
-                        <label htmlFor="listing">FIND LISTING</label>
-                        <input type="text" name="listing" id="listing" placeholder="Cape Verdean Food" />
-                    </div>
-
-                    <div>
-                        <label htmlFor="category">SELECT CATEGORY</label>
-                        <select name="category" id="category" title="category">
-                            <option value="a">Aaa</option>
-                            <option value="b">Bbb</option>
-                            <option value="c">Ccc</option>
-                        </select>
-                    </div>
-                    <div>
-                        <label htmlFor="location">SEARCH BY LOCATION</label>
-                        <input type="text" id="location" name="location" placeholder="Zip 02806" />
-                    </div>
-                    <div>
-                        <a href="/"><small>CLEAR ALL</small></a>
-                        <button>APPLY</button>
-                    </div>
-                </BusinessesFilter>
-
                 <AllBusinesses>
-                {
-                    /** Where to map the businesses. Info will be populated from an xhr value from useEffect */
-                    GenerateCards(allBusinesses)
-                }
+                    <BusinessesFilter>
+                        <div>
+                            <label htmlFor="listing">FIND LISTING</label>
+                            <input type="text" name="listing" id="listing" placeholder="Cape Verdean Food" />
+                        </div>
+
+                        <div>
+                            <label htmlFor="category">SELECT CATEGORY</label>
+                            <select name="category" id="category" title="category">
+                                <option value="a">Aaa</option>
+                                <option value="b">Bbb</option>
+                                <option value="c">Ccc</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label htmlFor="location">SEARCH BY LOCATION</label>
+                            <input type="text" id="location" name="location" placeholder="Zip 02806" />
+                        </div>
+                        <div>
+                            <a href="/"><small>CLEAR ALL</small></a>
+                            <button>APPLY</button>
+                        </div>
+                    </BusinessesFilter>
+
+                    { GenerateCards(allBusinesses) }
                 </AllBusinesses>
             </section>
 
-            <button type="button">SEE MORE BUSINESSES</button>
+            <button type="button" onClick={retrieveBusinesses}>SEE MORE BUSINESSES</button>
         </ViewAllBusinessesContainer>
     );
 };
