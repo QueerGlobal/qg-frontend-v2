5e749004fb14dbc15ead36c3b21d0993df227020
Meke Brown <mekesiabrown@gmail.com>
2022-07-11 06:54:04 -0400

    style: remove width for each card, controlled in parent styling

--- src/components/business/view-all/BusinessCard.css.ts
+++ src/components/business/view-all/BusinessCard.css.ts
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 export const BusinessCardContainer = styled.div`
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     flex: 1;
-    width 33%; 
 `;
 
 export const Thumbnail = styled.img`
