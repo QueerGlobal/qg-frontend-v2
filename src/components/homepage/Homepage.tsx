15e193cc0ac9edb87b2530443f68fd6cc73d2dca
Meke Brown <mekesiabrown@gmail.com>
2022-08-21 12:22:22 -0400

    refactor: update homepage components

--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -1,16 +1,21 @@
 import React, { FC } from 'react';
-
 import { PageWrapper } from './Homepage.css';
-import HomepageCTA from '../homepageCTA/HomepageCTA';
-import HomepageLinks from '../homepageLinks/HomepageLinks';
-import HomepageCTA2 from '../homepageCTA2/HomepageCTA2';
+import HomepageContents from '../homepageContents/HomepageContents';
+import BackgroundShapes from '../common/home/BackgroundShapes';
+import ForegroundShapes from '../common/home/ForegroundShapes';
+import HiGreeting from './HiGreeting';
+import BottomImages from './BottomImages';
+
 const Homepage: FC = () => {
   return (
     <PageWrapper>
-        <HomepageCTA />
-        <HomepageLinks />
-        <HomepageCTA2 />
+      <BackgroundShapes />
+      <HiGreeting />
+      <HomepageContents />
+      <ForegroundShapes />
+      <BottomImages />
     </PageWrapper>
   );
 };
+
 export default Homepage;
