3579fd49b13515cca1fca93b0e9eac7859a38eeb
colin <colin.williams91@gmail.com>
2023-05-01 11:54:53 -0500

    link socials urls

--- src/components/common/social-icons/SocialIcons.tsx
+++ src/components/common/social-icons/SocialIcons.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from "react";
 import {
-	SocialContainer,
-	IconsWrapper,
-	IconContainer,
+  SocialContainer,
+  IconsWrapper,
+  IconContainer,
 } from "./SocialIcons.css";
 import facebook from "../../../assets/footer/facebook.svg";
 import twitter from "../../../assets/footer/twitter.svg";
@@ -11,37 +11,57 @@ import linkedIn from "../../../assets/footer/linkedIn.svg";
 import pinterest from "../../../assets/footer/pinterest.svg";
 
 const SocialIcons: FC = () => {
-	return (
-		<SocialContainer>
-			<IconsWrapper>
-				<IconContainer className="svg">
-					<a href="/" target="_blank" rel="noreferrer">
-						<img src={facebook} alt="facebook" />
-					</a>
-				</IconContainer>
-				<IconContainer>
-					<a href="/" target="_blank" rel="noreferrer">
-						<img src={twitter} alt="twitter" />
-					</a>
-				</IconContainer>
-				<IconContainer>
-					<a href="/" target="_blank" rel="noreferrer">
-						<img src={instagram} alt="instagram" />
-					</a>
-				</IconContainer>
-				<IconContainer>
-					<a href="/" target="_blank" rel="noreferrer">
-						<img src={linkedIn} alt="linkedIn" />
-					</a>
-				</IconContainer>
-				<IconContainer>
-					<a href="/" target="_blank" rel="noreferrer">
-						<img src={pinterest} alt="facebook" />
-					</a>
-				</IconContainer>
-			</IconsWrapper>
-		</SocialContainer>
-	);
+  return (
+    <SocialContainer>
+      <IconsWrapper>
+        <IconContainer className="svg">
+          <a
+            href="https://www.facebook.com/QueerGlobal/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={facebook} alt="facebook" />
+          </a>
+        </IconContainer>
+        <IconContainer>
+          <a
+            href="https://twitter.com/QueerGlobal_"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={twitter} alt="twitter" />
+          </a>
+        </IconContainer>
+        <IconContainer>
+          <a
+            href="https://www.instagram.com/queerglobal/?hl=en"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={instagram} alt="instagram" />
+          </a>
+        </IconContainer>
+        <IconContainer>
+          <a
+            href="https://www.linkedin.com/company/queer-global/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={linkedIn} alt="linkedIn" />
+          </a>
+        </IconContainer>
+        <IconContainer>
+          <a
+            href="https://www.pinterest.com/QueerGlobal/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={pinterest} alt="facebook" />
+          </a>
+        </IconContainer>
+      </IconsWrapper>
+    </SocialContainer>
+  );
 };
 
 export default SocialIcons;
