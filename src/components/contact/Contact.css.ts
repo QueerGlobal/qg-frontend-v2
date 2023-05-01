9e0a019969b3a8ebd1672db39f4a3638e1b2a3e9
colin <colin.williams91@gmail.com>
2023-05-01 18:41:26 -0500

    re-styling for wireframes

--- src/components/contact/Contact.css.ts
+++ src/components/contact/Contact.css.ts
@@ -1,11 +1,40 @@
 import styled from 'styled-components';
+import colors from '../../styles/colors';
+import Button from "../common/button/Button";
 
-/* #655DC6 for purple */
-/* #F5ACB8 for pink */
+/* #FFFFFF for white panels and CONTACT text */
+/* #655DC600 for white gradient 1 */
+/* #FFFFFF00 for white gradient 2 */
+
+/* #655DC6 for medium purple */
+/* #655DC6CC for light purple */
+
+/* #1F5EA9 for dark blue */
+/* #2A7DE1 for medium blue */
+/* #54C0E8 for medium cyan/ring */
+
 /* #1F1F1F for dark gray bg */
-/* #764D25 for brown gradient in footer */
-/* #C59B6C for light brown gradient in footer */
-/* #48ADE5 for blue circle on home page */
+/* #1F1F1F for dark gray text */
+/* #B5B5B5 for gray text boxes */
+/* #898C8E for gray text placeholder */
+/* #00000029 for gray button borders */
+/* #E0E0E0 for gray FAQ vectors */
+
+/* #231F20 for black circle */
+/* #000000 for black rings */
+/* #161616 for black FAQ text */
+
+/* #E50695 for pink donate button */
+
+export const ContactWrapper = styled.div`
+ background-color: ${colors.WHITE};
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+ justify-content: center;
+ padding: 15px;
+ width: 95%;
+`;
 
 export const ContactMain = styled.main`
   display: flex;
@@ -13,7 +42,15 @@ export const ContactMain = styled.main`
   margin: 10%;
 `;
 
-export const WhiteBGSection = styled.div`
+export const WhiteBGSectionLeft = styled.div`
+  background-color: white;
+  display: grid;
+  justify-content: center;
+  justify-items: center;
+  text-align: center;
+`;
+
+export const WhiteBGSectionRight = styled.div`
   background-color: white;
   display: grid;
   justify-content: center;
@@ -49,3 +86,16 @@ export const ButtonWrapper = styled.div`
   gap: 5rem;
   width: 70%;
 `;
+
+export const SendMessageButton = styled(Button)`
+/* Layout Properties */
+/* top: 2390px;
+left: 1086px;
+width: 278px;
+height: 55px; */
+/* UI Properties */
+background: transparent linear-gradient(90deg, var(--unnamed-color-54c0e8) 0%, var(--unnamed-color-2a7de1) 100%) 0% 0% no-repeat padding-box;
+background: transparent linear-gradient(90deg, #54C0E8 0%, #2A7DE1 100%) 0% 0% no-repeat padding-box;
+box-shadow: 0px 3px 6px #00000029;
+opacity: 1;
+`
--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -2,44 +2,40 @@ import React, { FC } from "react";
 
 import Footer from "../common/footer/Footer";
 import {
-  WhiteBGSection,
+  WhiteBGSectionLeft,
+  WhiteBGSectionRight,
   ContactMain,
   TextWrapper,
   TextArea,
   ButtonWrapper,
   PurpleSpan,
+  ContactWrapper,
+  SendMessageButton,
 } from "./Contact.css";
 import Button from "../common/button/Button";
 
 const Contact: FC = () => {
   return (
     <>
-      <ContactMain id="main">
-        <WhiteBGSection id="white-bg-section">
+      <ContactWrapper id="main">
+        <WhiteBGSectionLeft id="white-bg-section">
           <h2>
             <PurpleSpan>CONTACT</PurpleSpan> US.
           </h2>
-          <p>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum."
-          </p>
-          <TextWrapper id="text-wrapper">
+          {/* <TextWrapper id="text-wrapper">
             <TextArea
               placeholder="Tell us what's on your mind..."
               id="text-area"
             />
-          </TextWrapper>
-          <ButtonWrapper>
-            <Button label="primary" buttonText="Send us your message" />
-            <Button label="primary" buttonText="Send us an Email" />
-          </ButtonWrapper>
-        </WhiteBGSection>
-      </ContactMain>
+          </TextWrapper> */}
+        </WhiteBGSectionLeft>
+        <ButtonWrapper>
+          <SendMessageButton
+            label="secondary"
+            buttonText="Send us your message"
+          />
+        </ButtonWrapper>
+      </ContactWrapper>
       <Footer />
     </>
   );
