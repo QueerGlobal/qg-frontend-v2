8fb3245fc0fca0d79b519291dff8091b0b1417a9
Mekesia Brown <mekesiabrown@gmail.com>
2023-05-02 23:31:21 -0400

    Remove Boilerplate Create React App Files and Code (#22)
    Remove Boilerplate Create React App Files and Code.
    
    Please email me at fssorcerer@queerglobal.com or send me a message in
    Slack if you need to. Thanks - Meke

--- src/Global.css.ts
+++ src/Global.css.ts
@@ -1,46 +1,50 @@
 import { createGlobalStyle } from "styled-components";
 import colors from './styles/colors';
 
-// TODO - Find out from Jenn about 'MYSTERIO'
 const GlobalStyles = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-  }
-  body {
-    margin: 0;
-    padding: 0;
-  }
+	* {
+		box-sizing: border-box;
+	}
 
-.dropdown-menu {
-	position: absolute !important;
-	left: 0 !important;
-	width: 100% !important;
-	background-color: ${colors.MYSTERIO} !important;
-	border-radius: 0 !important;
-}
+	body {
+		margin: 0;
+		padding: 0;
+	}
 
-.nav-link {
-	color: ${colors.WHITE} !important;
-	letter-spacing: 34;
-	padding: 0.5rem 0;
-}
+	code {
+		font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace;
+	}
 
-.offcanvas-body svg {
-	width: 1.125rem !important;
-	height: 1em;
-	margin-left: 20px;
-}
+	a[aria-expanded="true"] {
+		background-color: ${colors.MYSTERIO};
+		width: 100%;
+		margin-bottom: 190px !important;
+	}
 
-a[aria-expanded="true"] {
-	background-color: ${colors.MYSTERIO};
-	width: 100%;
-	margin-bottom: 190px !important;
-}
+	svg {
+		stroke: ${colors.BLUE};
+		fill: ${colors.WHITE} !important;
+	}
 
-svg {
-	stroke: ${colors.BLUE};
-	fill: ${colors.WHITE} !important;
-}
+	.dropdown-menu {
+		position: absolute !important;
+		left: 0 !important;
+		width: 100% !important;
+		background-color: ${colors.MYSTERIO} !important;
+		border-radius: 0 !important;
+	}
+
+	.nav-link {
+		color: ${colors.WHITE} !important;
+		letter-spacing: 34;
+		padding: 0.5rem 0;
+	}
+
+	.offcanvas-body svg {
+		width: 1.125rem !important;
+		height: 1em;
+		margin-left: 20px;
+	}
 `;
 
 export default GlobalStyles;
