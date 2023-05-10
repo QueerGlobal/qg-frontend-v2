904529a904dea933000aa7b2a81b26a1ccb3e63f
Mekesia Brown <mekesiabrown@gmail.com>
2023-05-10 09:56:56 -0400

    Convert app.js Router File to app.tsx (#24)
    Continuation of TypeScript conversion in #20 - Convert app.js Router
    File to app.tsx
    
    Convert app.js to app.tsx, which needed the package
    @types/react-router-dom. This unfortunately depends on @types/history
    which is deprecated. [There is discussion about removing
    it](https://github.com/DefinitelyTyped/DefinitelyTyped/pull/58216) but
    as of now the package is still a dependency. @types/react-router-dom
    also [holds @types/react-router as a
    dependency](https://www.npmjs.com/package/@types/react-router-dom).
    
    Please Slack me for anything! - Meke

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
-import Donate from "./components/donate/Donate.tsx";
-import Profile from "./components/profile/Profile.tsx";
-import Blog from "./components/blog/Blog.tsx";
-import AddResource from "./components/add-resource/AddResource.tsx";
-import Search from "./components/search/Search.tsx";
+import Donate from "./components/donate/Donate";
+import Profile from "./components/profile/Profile";
+import Blog from "./components/blog/Blog";
+import AddResource from "./components/add-resource/AddResource";
+import Search from "./components/search/Search";
 import Logout from "./components/logout/Logout";
-import GlobalStyles from "../src/Global.css";
+import GlobalStyles from "./Global.css";
 import TopNav from "./components/common/topnav/TopNav";
 
-const App = () => {
-	const windowWidth = window.innerWidth <= 375;
+const App = (): JSX.Element => {
+	const windowWidth = window.innerWidth <= 375; // TODO: Delete windowWidth; Moved to TopNav
 
 	return (
 		<>
