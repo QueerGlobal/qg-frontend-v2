f96e9e08d03575f48f05b6ed80a90d2223e68d0e
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-07-28 12:46:05 -0400

    fixes logo and donation method display as well as adds pie chart cards of where donations go

--- src/components/donate/DonateCard.js
+++ src/components/donate/DonateCard.js
@@ -4,12 +4,13 @@ import './Donate.css'
 
 export const DonateCard = ( method ) => {
 
+console.log(method)
 
 
     return (
     <div className="pay-option-card">
-      <img  src={method.logo} className="pay-option-logo" />
-        <p>{method.name}</p>
+      <img  src={method.method.logo} className="pay-option-logo" />
+        <p className="pay-option-name">{method.method.name}</p>
       <button>DONATE</button>
     </div>
   );
--- src/components/donate/config.js
+++ src/components/donate/config.js
@@ -1,11 +1,11 @@
+
 import goFundMe from './logos/gofundme.png'
 import patreon from './logos/patreon.png'
 import venmo from './logos/venmo.png'
 import cashApp from './logos/cashApp.png'
 import paypal from './logos/paypal.png'
-import creditCard from './logos/creditCard.png'
+import cc from './logos/cc.png'
 import amazon from './logos/amazon.png'
-
 
 
 
@@ -27,7 +27,7 @@ export const donationMethods = [
     logo: paypal,
 },
 {   name: 'ACH',
-    logo: 'creditCard',
+    logo: cc,
 },
 {   name: 'Amazon Wish List',
     logo: amazon,
